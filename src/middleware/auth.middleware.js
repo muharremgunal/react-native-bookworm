eb3c1442e42ddf52a854dc6a189250bc7b4b81d9
Muharrem Günal <muharremgunal@Muharrems-MacBook-Air.local>
2025-04-25 03:30:34 +0300

    first commit

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer", "");
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "No authentication token, access denied" });
+
+    const decoded = jwt.verify(token, process.env.JWT.SECRET);
+
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(401).json({ message: "Token is not valid" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error creating book", error);
+    res.status(500).json({ message: "Token is not valid" });
+  }
+};
+
+export default protectRoute;
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,94 @@
+import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
+
+router.post("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password should be at least 6 characters long" });
+    }
+    if (username < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username should be at least 3 characters long" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    const existingUsername = await User.findOne({ username });
+
+    if (existingEmail)
+      return res.status(400).json({ message: "Email already exists" });
+    if (existingUsername)
+      return res.status(400).json({ message: "Username already exists" });
+
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+    const user = new User({
+      email,
+      username,
+      password,
+      profileImage,
+    });
+
+    await user.save();
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Error in register route", error);
+    res.status(500).json({ message: "Internaş server error" });
+  }
+});
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ message: "User does not exist" });
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
