bc7dda31a8ab7ae0183191be53cd9575414eda83
Muharrem Günal <muharremgunal@Muharrems-MacBook-Air.local>
2025-04-25 03:56:45 +0300

    add cron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import job from "./lib/cron.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import booksRoutes from "./routes/booksRoutes.js";
@@ -9,6 +10,7 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
